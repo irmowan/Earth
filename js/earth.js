4df77fe7ee7995570c7daaf84dee322e272ba00e
irmowan <irmowan@gmail.com>
2016-11-26 23:46:16 +0800

    Complete the earth

--- js/earth.js
+++ js/earth.js
@@ -5,31 +5,37 @@
 var WIDTH = window.innerWidth - 30,
     HEIGHT = window.innerHeight - 30;
 
-var angle = 60,
+var angle = 45,
     aspect = WIDTH / HEIGHT,
     near = 0.1,
-    far = 3000;
+    far = 10000,
+    rotationSpeed = 0.2;
+var clock = new THREE.Clock();
 
 var container = document.getElementById('container');
 
 var camera = new THREE.PerspectiveCamera(angle, aspect, near, far);
-camera.position.set(0, 0, 0);
+camera.position.set(1200, 0, 0);
+
 var scene = new THREE.Scene();
+camera.lookAt(scene.position);
 
 var light = new THREE.SpotLight(0xFFFFFF, 1, 0, Math.PI / 2, 1);
-light.position.set(4000, 4000, 1500);
-light.target.position.set(1000, 3800, 1000);
+light.position.set(4000, 4000, 2500);
+light.target.position.set(1000, 1000, 5000);
+light.castShadow = true;
 scene.add(light);
 
-var earthGeo = new THREE.SphereGeometry(30, 40, 400),
+var earthGeo = new THREE.SphereGeometry(200, 400, 400),
     earthMat = new THREE.MeshPhongMaterial();
-earthMat.map = THREE.ImageUtils.loadTexture('img/earth.jpg');
+earthMat.map = THREE.ImageUtils.loadTexture('img/earth2880.jpg');
 
 var earthMesh = new THREE.Mesh(earthGeo, earthMat);
 earthMesh.position.set(-100, 0, 0);
 earthMesh.rotation.y = 5;
+earthMesh.castShadow = true;
+earthMesh.receiveShadow = true;
 scene.add(earthMesh);
-camera.lookAt(earthMesh.position);
 
 var starGeo = new THREE.SphereGeometry(3000, 10, 100),
     starMat = new THREE.MeshBasicMaterial();
@@ -41,14 +47,23 @@ scene.add(starMesh);
 
 var renderer = new THREE.WebGLRenderer({antialiasing: true});
 renderer.setSize(WIDTH, HEIGHT);
-renderer.domElement.style.position = 'relative';
 
 container.appendChild(renderer.domElement);
-renderer.autoClear = false;
-renderer.shadowMapEnabled = true;
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+}
 
 function render() {
+    var delta = clock.getDelta();
+    earthMesh.rotation.y += rotationSpeed * delta;
+    renderer.clear();
     renderer.render(scene, camera);
 }
 
-render();
+animate();
